0ffca0827fadaef3c434024e72bf17b97f0ac876
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-16 08:38:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     
-     puts programmer_hash[:dennis_ritchie][:languages]
+     language = ""
+     language = programmer_hash[:dennis_ritchie][:languages]
+     return language
 end
 
 def adding_matz
